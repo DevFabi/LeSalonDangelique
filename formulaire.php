74173dbccd855ebeba0210e8169836f39f6386c4
Fabiola BELET <fabiola.belet@gmail.com>
2018-12-05 13:50:37 -0500

    first commit

---
+++ formulaire.php
@@ -0,0 +1,437 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Le salon d'Angélique</title>
+
+
+
+	<link href="style.css" rel="stylesheet">
+    <link href="bootstrap.min.css" rel="stylesheet">
+	<link href="dist/jquery.fatNav.css" rel="stylesheet" type="text/css">
+    
+	
+			
+		
+  </head>
+  <body>
+
+  
+  
+<div class="fat-nav">
+  <div class="fat-nav__wrapper">
+    <ul>
+        <li><a href="">Accueil</a></li>
+        <li><a href="#Lesalon">Le salon</a></li>
+        <li><a href="#lequipe">L'équipe</a></li>
+        <li><a href="#prestations">Prestations</a></li>
+		<li><a href="#produits">Produits</a></li>
+        <li><a href="#contact">Contact</a></li>
+    </ul>
+  </div>
+</div>
+
+<div class="jquery-script-ads" align="center">
+
+</script></div>
+<script src="http://code.jquery.com/jquery-2.1.3.min.js"></script> 
+<script src="dist/jquery.fatNav.js"></script> 
+<script>
+(function() {
+    
+    $.fatNav();
+    
+}());
+</script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+  
+  
+  
+    <div class="container-fluid">
+	<div class="row">
+	
+		
+		
+		
+		<div class="col-md-12">
+		
+		<nav id="nav-main">
+		<div class="hidden-xs"><h1><img src="images/LOGOA.png" alt="..."/></h1><br></div>
+		<div class="visible-xs"><h1><img src="images/PETITlogosalon.png" alt="..."/></h1><br></div>
+		
+
+		</nav>
+		<div id="nav-trigger">
+    <span>Menu</span>
+		</div>
+
+
+			<div class="row">
+				<div class=" col-md-12">
+<div id="Lesalon">
+				<div class="section1">	
+				<div class="row">
+					<div class="col-md-6 hidden-xs">
+<p><img src="images/angigif.gif" alt="..." ></p>
+					</div>
+					
+					<div class=" col-md-6">
+						
+					<div class="hidden-xs">
+
+					<div class="titredebut"><h1><br>Le salon d'angélique</h1> <br><br><br><br></div>
+						<div class="pdebut"><p>
+								Angélique et son équipe vous accueillent dans un cadre
+
+original, chaleureux et relaxant pour se détendre et prendre
+
+enfin soin de vous. Avec son partenaire DAVINES, les
+
+équipes du salon vous proposeront des produits de qualité
+
+avec une approche authentique et personnalisée, favorisant
+
+brillance et couleur naturelle et durable. Le salon
+
+d’Angélique  c'est aussi un espace hair spa que vous pourrez découvrir avec un massage de tête pour se déstresser mais aussi entretenir la beauté
+
+naturelle de vos cheveux. Enfin, l'espace bien-être vous
+
+offrira un pur moment de détente avec les produits
+
+COMFORT ZONE </p> <br>
+
+<p><span id="motangi">Marie-Line, Naima et Angélique vous invitent à venir vous
+
+déconnecter le temps d'un agréable moment, dans une
+
+ambiance musicale conviviale. </span></p>     
+
+                       
+						<br>
+						<i><p><span id ="signature">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Angélique Vincendeau </span></p><i></div>
+						</div>
+						
+<div class="visible-xs">
+
+
+<div class="titredebutXS"><h1>Le salon d'angélique</h1> <br></div>
+						<div class="pdebutXS"><p>
+								Angélique et son équipe vous accueillent dans un cadre
+
+original, chaleureux et relaxant pour se détendre et prendre
+
+enfin soin de vous. Avec son partenaire DAVINES, les
+
+équipes du salon vous proposeront des produits de qualité
+
+avec une approche authentique et personnalisée, favorisant
+
+brillance et couleur naturelle et durable. Le salon
+
+d’Angélique  c'est aussi un espace hair spa que vous pourrez découvrir avec un massage de tête pour se déstresser mais aussi entretenir la beauté
+
+naturelle de vos cheveux. Enfin, l'espace bien-être vous
+
+offrira un pur moment de détente avec les produits
+
+COMFORT ZONE </p> <br>
+
+<p><span id="motangiXS">Marie-Line, Naima et Angélique vous invitent à venir vous
+
+déconnecter le temps d'un agréable moment, dans une
+
+ambiance musicale conviviale. </span></p>    
+<i><p><span id ="signatureXS">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Angélique Vincendeau </span></p><i>
+
+
+</div>
+					</div>
+				</div>
+				</div>
+				</div>
+</div>
+				<br><br>
+				<section><div class="row">
+<div id="lequipe">
+					<div class=" col-md-12">
+					<br>
+					<div class="equipe">
+					<br><h1>L'équipe</h1><br><br>
+					<div class="col-md-4">
+					<img src="images/f1.png" alt="..." class="img-circle">
+					<h3>Angélique</h3>
+					<p>Passionnée depuis toujours par la couleur, j’ai eu envie d’ouvrir mon propre salon
+
+après une belle décennie passée dans le 16ème arrondissement en étroite
+
+collaboration avec la marque DAVINES. J’attache une importance toute particulière
+
+au bien être de mes clients, surtout au moment du shampoing en exécutant de
+
+fabuleux massages du cuir chevelu. L’essentiel est pour moi de travailler dans une
+
+belle atmosphère alliant plaisir, convivialité et envie de réaliser des colorations
+
+personnalisées. En ouvrant le Salon d’Angélique, j’ai eu envie de créer un endroit qui
+
+me ressemble, à la fois authentique et où le bien être est le maitre mot, loin de
+
+l’agitation parisienne.</p>
+					</div>
+					<div class="col-md-4">
+					<img src="images/f2.png" alt="..." class="img-circle">
+					<h3>Naïma</h3>
+					<p>Après avoir travaillé quelques années avec Naïma non loin du Trocadéro, c’était comme
+
+une évidence de la prendre dans mon équipe. Généreuse autant personnellement que
+
+professionnellement, elle a un véritable don pour les massages qu’elle décrit souvent
+
+comme un voyage.</p>
+					</div>
+					<div class=" col-md-4">
+					<img src="images/f3.png" alt="..." class="img-circle">
+					<h3>Marie-line</h3>
+					<p>Je n’ai pas eu à chercher bien longtemps la personne qui allait travailler à mes côtés.
+
+En effet Marie- Line et moi nous connaissons depuis de nombreuses années. Fidèle en
+
+amitié et excellente coupeuse, elle est expérimentée et passionnée par son métier.
+
+Soucieuse du moindre détail, elle exécute toutes ses coupes sur cheveux
+
+secs afin d’obtenir le meilleur résultat possible..</p>
+					</div>
+					</div>
+					</div>
+</div>
+				</div> </section>
+
+
+<div id="prestations">				
+	<div class="prestations">
+		<div class="row">
+			<div class="col-md-12">
+				<h1><br> Prestations </h1><br><br>
+			<div class=" col-md-6">
+			<div class="tarifs">
+				
+				<p> Toutes nos prestations incluent le shampoing et le soin Davines <br>Supplément cheveux très longs sur devis. <br><br>
+				<span id="tarifs"><strong>ESPACE COLORATION</strong> <br><br></span>
+				
+				Coloration Mask 55€ <br>
+				Balayage + patine 105€ <br>
+				Flash effet lumière + patine 75€ <br>
+				Forfait couleur + coupe 110€<br>
+				Transformation couleur 140€ <br><br>
+				
+				<span id="tarifs"><strong>ESPACE COIFFAGE  </strong> </span><br><br>
+				
+				Coupe FEMME 40€<br>
+				Brushing  30€<br>
+				Attache minute 20€<br>
+				Attache 40€<br>
+				Chignon  70€<br>
+				Transformation coupe 70€<br><br>
+				Coupe HOMME 40€<br>
+				Barbe 20€<br>
+				</p>
+				
+			</div>
+			</div>
+			
+			
+			<div class=" col-md-6">
+			<div class="tarifs">
+			
+			<p>
+			
+			
+			<span id="tarifs"><strong>HAIR SPA</strong><br><br></span>
+Rituel bien être du cheveu   <br><br>
+
+
+rituel detoxifiant 45€   <br>
+rituel nourrissant 45€   <br>
+rituel repulpant 45€   <br>
+rituel éclat de jeunesse 55€   <br>
+rituel masculin 40€   <br><br>
+
+rituel expérience 65€ durée 60 minutes 
+compatible avec les rituels ci dessus. <br><br>
+			
+			
+			 <span id="tarifs"><strong>MASSAGES</strong><br><br></span>
+			Massage relaxant - Massage tonique - Massage balinais<br>
+			1H00 89€<br>
+			1H15 105€<br>
+			1H30 130€<br>
+			Gommage + massage (1H30) 130€<br><br>
+			Exclusivement sur rendez-vous
+			</p></div>
+			</div>
+			
+			
+			
+</div>
+			</div>
+			</div>
+		</div>
+	</div>
+</div>
+			
+			
+			
+
+<div id="produits">
+				<div class="sectionproduit">
+				<div class="row">
+				<div class=" col-md-12">
+				<br><br><h1>Produits</h1><br><br>
+
+				
+					<div class=" col-md-6">
+					<p><img src="images/davinesproduit.png" alt="..." ></p>	
+					<br><p>Davines est une marque de cosmétiques italienne qui fabrique des produits de haute qualité dans le respect de l'environnement. 
+Leurs couleurs sont ultra naturelles, brillantes. Grâce à eux et à notre savoir faire ,nous obtenons de magnifiques résultats. 
+Quant à leurs shampooings et soins, c'est un réel plaisir de travailler avec toutes leurs gammes. 
+Le cheveu est soyeux, doux, brillant. Il retrouve une seconde jeunesse. 
+Avec les différents programmes de soins proposés dans le hair Spa, vous pourrez apprécier les résultats sur votre chevelure. 
+Vous serez détendus avec les rituels de massage capillaire crées spécialement pour un moment de bien être unique.</p>
+<p>"  EN CRÉANT DE LA "BEAUTÉ DURABLE", NOUS VOULONS ENCOURAGER LES GENS À PRENDRE SOIN D'EUX, DE L'ENVIRONNEMENT DANS LEQUEL ILS VIVENT ET TRAVAILLENT, ET DES CHOSES QU'ILS AIMENT. ”</p>
+							
+					<br></div>	
+						<div class="col-md-6">
+					<div class="logocenter"><p><div class="hidden-xs"><img src="images/comfortz.png" alt="..." ></div></p>	
+					<p><div class="visible-xs"><img src="images/comfortzPETIT.png" alt="..." ></div></p></div>
+					<br><p>Comfort zone à été conçue et lancée par le groupe DAVINES.
+Le résultat est un système complet de produits de soin innovant pour la peau et le bien être du corps et de l esprit. 
+Tous leurs produits sont conçus à Parme en Italie avec 100%d énergies renouvelables. 
+Nous adorons leurs visions de la vie et du monde en général. 
+c'est aussi pour cela que nous avons choisit de travailler avec eux.
+Nous vous laissons le soin de decouvrir comfort zone dans notre espace dédié au bien être en compagnie 
+de Naima.  </p>
+						<br></div>
+						
+								
+					
+				</div>	
+				</div>
+</div>
+</div>
+			
+			
+															
+  </body>
+ 
+
+
+ 
+ <footer>
+
+ 
+ 
+ <div id="contact">
+				<div class="sectioncontact">
+				<div class="row">
+				<div class="hidden-xs col-md-12">
+				<br><br><h1>Contact</h1><br><br>
+
+				
+					<div class="col-xs-4 col-md-4">
+					
+					<br></div>	
+						<div class="col-xs-4 col-md-4">
+						 
+	  <?php
+$TO = " contact@lesalondangelique.com";
+
+$h  = "From: " . $TO;
+
+$message = "";
+
+while (list($key, $val) = each($_POST)) {
+  $message .= "$key : $val\n";
+}
+
+mail($TO, $subject, $message, $h);
+
+Header("Location: http://fabiolabelet.com/angi/formulaire.php");
+
+?>
+<p> Votre message a bien été envoyé <p><br><br>
+	   </div>
+	   
+						<br></div>
+						<div class="col-xs-4 col-md-4">
+					
+							
+					</div>
+						
+								
+					
+				</div>	
+				</div>
+</div>
+
+ 
+ 
+
+<div id="contact"> 
+  	<div class="row">
+					<div class="col-xs-6 col-md-6">
+						
+							<address>
+								 <p class="semibold text-uppercase" style="font-size:35px; letter-spacing: 0.1em; color: black;"><br>Horaires</p>
+								 <br>
+								 sur rendez-vous<br> 
+lundi au samedi de 10h à 18 h 30 <br>
+nocturne jeudi jusqu'à 20 h<br><br>
+								 <p><a href="http://www.facebook.com/lesalondangelique"><img src="images/lesalondangelique-f.png" alt="..."></a><a href="https://fr.pinterest.com/salondangelique/"><img src="images/lesalondangelique-p.png" alt="..." ></p>
+							<a href="mentions.html">Mentions légales</a>    </address>
+					</div>	
+						<div class="col-xs-6 col-md-6">
+							 
+							<address>
+								  <br><br><div class="hidden-xs"><p><img src="images/PETITLOGOH.png" alt="..." ></p></div>
+								  <div class="visible-xs"><p><img src="images/PETITLOGO.png" alt="..."/></p></div>
+								  <p><img src="images/davines.png" alt="..." ></p>
+								  4 Avenue de Messine<br> PARIS, 75008<br> <br> <abbr title="Phone">Tel:</abbr> 01.45.62.17.63
+							</address>
+						
+						</div>
+						
+								
+					
+				</div>
+				
+</div>
+  </footer>
+				
+			</div>
+		</div>
+	</div>
+</div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+</html>
